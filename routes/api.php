ac49019bbaa9d55900ca90700d8ddbf95a4d5e2d
mrmp <mohamadpouran@gmail.com>
2022-02-12 09:39:59 +0330

    login and logout with api token and get current user data

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\Authcontroller;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -21,15 +22,26 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::get('/articles', [ArticleController::class, 'index'])->name('articles.list');
-Route::post('/articles', [ArticleController::class, 'store'])->name('article.store');
-Route::get('/article/{id}', [ArticleController::class, 'show'])->name('article.show');
-Route::put('/article/{articleId}', [ArticleController::class, 'update'])->name('article.update');
-Route::delete('/article/{articleId}', [ArticleController::class, 'remove'])->name('article.remove');
+Route::group([], function () {
+    Route::post('/login', [Authcontroller::class, 'login'])->name('login');
+    Route::post('/logout', [Authcontroller::class, 'logout'])->middleware('auth:api')->name('logout');
+    Route::get('/user-me', [Authcontroller::class, 'user'])->middleware('auth:api')->name('current.user');
+});
 
+Route::group(['prefix' => 'users'], function () {
+//    Route::get('/', [UserController::class, 'index'])->middleware('auth:api')->name('users');
+    Route::get('/', [UserController::class, 'index'])->name('users');
+    Route::post('/', [UserController::class, 'store'])->name('user.store');
+    Route::get('/{id}', [UserController::class, 'show'])->name('user.show');
+    Route::put('/{userId}', [UserController::class, 'update'])->name('user.update');
+    Route::delete('/{userId}', [UserController::class, 'remove'])->name('user.remove');
+});
+Route::group(['prefix' => 'articles'], function () {
 
-Route::get('/users', [UserController::class, 'index'])->name('users');
-Route::post('/users', [UserController::class, 'store'])->name('user.store');
-Route::get('/user/{id}', [UserController::class, 'show'])->name('user.show');
-Route::put('/user/{userId}', [UserController::class, 'update'])->name('user.update');
-Route::delete('/user/{userId}', [UserController::class, 'remove'])->name('user.remove');
+    Route::get('/', [ArticleController::class, 'index'])->name('articles.list');
+    Route::post('/', [ArticleController::class, 'store'])->name('article.store');
+    Route::get('/{id}', [ArticleController::class, 'show'])->name('article.show');
+    Route::put('/{articleId}', [ArticleController::class, 'update'])->name('article.update');
+    Route::delete('/{articleId}', [ArticleController::class, 'remove'])->name('article.remove');
+
+});
